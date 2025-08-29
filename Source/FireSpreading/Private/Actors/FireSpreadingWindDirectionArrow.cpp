9446f32f2c693fbb0f69ddf15a8407c307e6bfed
Goga Akobiya <googlichek.one@gmail.com>
2025-08-29 16:44:46 +0400

    Some tweaks and helpers.

---
+++ Source/FireSpreading/Private/Actors/FireSpreadingWindDirectionArrow.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/FireSpreadingWindDirectionArrow.h"
+
+#include "Game/FireSpreadingGameInstance.h"
+
+
+// Sets default values
+AFireSpreadingWindDirectionArrow::AFireSpreadingWindDirectionArrow()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AFireSpreadingWindDirectionArrow::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void AFireSpreadingWindDirectionArrow::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+UFireSpreadingGameInstance* AFireSpreadingWindDirectionArrow::GetFireSpreadingGameInstance()
+{
+	if (FireSpreadingGameInstance)
+	{
+		return FireSpreadingGameInstance;
+	}
+
+	if (const UWorld* World = GetWorld())
+	{
+		if (UGameInstance* GameInstance = World->GetGameInstance())
+		{
+			FireSpreadingGameInstance = Cast<UFireSpreadingGameInstance>(GameInstance);
+		}
+	}
+
+	return FireSpreadingGameInstance;
+}
+
---
+++ Source/FireSpreading/Public/Actors/FireSpreadingWindDirectionArrow.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/FireSpreadingActorBase.h"
+#include "FireSpreadingWindDirectionArrow.generated.h"
+
+class UFireSpreadingGameInstance;
+
+UCLASS()
+class FIRESPREADING_API AFireSpreadingWindDirectionArrow : public AFireSpreadingActorBase
+{
+	GENERATED_BODY()
+
+public:
+	AFireSpreadingWindDirectionArrow();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	UFireSpreadingGameInstance* GetFireSpreadingGameInstance();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Transient)
+	TObjectPtr<UFireSpreadingGameInstance> FireSpreadingGameInstance = nullptr;
+};
